0a74ce59f6e4ba38dacde4b3615e367ebd2ad4e7
idesheaven <marianancerutti@gmail.com>
2023-02-09 10:47:17 -0300

    Atualizações JS

---
+++ exercicios/ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 33.8 
+console.log('Sua velocidade foi de: ' + vel)
+if (vel >= 60){
+    console.log('Você foi multado!')
+}
+console.log('Dirija sempre em segurança.')
---
+++ exercicios/ex009/index.js
@@ -0,0 +1,8 @@
+var n1 = 4
+console.log('o número escolhido foi: ' + n1)
+if (n1 == 2 ** 2) { // VARIÁVEL VEM ANTES DO IGUAL
+    console.log('número igual a dois elevado na segunda potência')
+}
+else{
+    console.log('número diferente de dois elevado na segunda potência')
+}
