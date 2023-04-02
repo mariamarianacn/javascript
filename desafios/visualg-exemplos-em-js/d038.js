a5ab79abf7ebc09403278544ab48d316d6586f2f
idesheaven <marianancerutti@gmail.com>
2023-04-02 13:11:56 -0300

    Atualizações JS

--- desafios/visualg-exemplos-em-js/d038.js
+++
@@ -1,5 +0,0 @@
-for (var n1 = 6 ; n1 <= 11 ; n1++){
-    console.log(n1)
-}
-console.log('acabou!')
-
--- desafios/visualg-exemplos-em-js/d040.js
+++
@@ -1,6 +0,0 @@
-var n1 = -3
-while (n1 < 18){
-    n1 = n1 + 3
-    console.log(n1)
-}
-console.log('chega!')
